8e19d5d61523f203ea66d9a9224ab824fe5e1e98
Danilo <danilo.santana26@Outlook.com.br>
2026-05-21 11:14:07 -0300

    docs: add comments

--- back-end/src/routes/usuarioRoutes.js
+++ back-end/src/routes/usuarioRoutes.js
@@ -1,11 +1,16 @@
+// importei o Router
 import { Router } from "express"
 
+// Importei as funções do usuarioController.js
 import { registerUsuario, loginUsuario } from "../controllers/usuarioController.js"
 
+//Router Criado
 const routerUsuario = Router()
 
+//Rota POST que leva ao registro de usuários
 routerUsuario.post('/usuarios/register', registerUsuario)
 
+//Rota POST que leva ao login de usuários
 routerUsuario.post('/usuarios/login', loginUsuario)
 
 export default routerUsuario
