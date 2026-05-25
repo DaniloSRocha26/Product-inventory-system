f4e299cdd4f3d6bf26c212bcaa5f83d75750d9d9
Danilo <danilo.santana26@Outlook.com.br>
2026-05-25 09:12:46 -0300

    feat: add createCategoria function to categoriaController

--- back-end/src/controllers/categoriaController.js
+++ back-end/src/controllers/categoriaController.js
@@ -1,5 +1,5 @@
 //Importo a função getAll do categoria.Service.js
-import { getAll } from "../services/categoriaService.js";
+import { create, getAll } from "../services/categoriaService.js";
 
 //Faço uma função assíncrona para pegar todas as categorias e devolver
 //como um json
@@ -15,4 +15,18 @@ async function getAllCategorias(req, res){
 
 }
 
-export {getAllCategorias}
+//Faço uma função assíncrona para pegar o nome da categoria, id do usuário
+//logado 
+//Criando a categoria no banco e devolvendo ela
+async function creatingCategoria(req, res){
+    try{
+        
+        //Chama o service passando o id do usuário e o nome, recebe a categoria de volta
+        const createCategoria = await create(req.user.id, req.body.nome)
+        res.status(201).json(createCategoria)
+    }catch(error){
+        res.status(500).json({ message: error.message})
+    }
+}
+
+export {getAllCategorias, creatingCategoria}
